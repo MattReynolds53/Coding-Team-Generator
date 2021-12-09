e0cbdc1bb1fdfed59cea98cb96fb808052a4cda3
Matt Reynolds <mattreynolds53@gmail.com>
2021-12-09 08:52:12 -0600

    Rearranged my code in index.js to fix issue that was bringing up all the questions for each category at once.

--- index.js
+++ index.js
@@ -16,196 +16,168 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const teamMembers = [];
 const idArray = [];
 
-function appMenu() {
+function appPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "What type of employee would you like to add first?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.start === "Manager") {
+        createManager();
+      } else if (answer.start === "Engineer") {
+        createEngineer();
+      } else if (answer.start === "Intern"){
+        createIntern();
+      } else {
+        buildTeam();
+      }
+    });
+}
+
   // Manager prompt to allow the user to add a manager
-  createManager();
-  function createManager() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your manager's name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your manager's employee ID number?",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your manager's current email address?",
-        },
-        {
-          type: "input",
-          name: "officeNum",
-          message: "What is your manager's office number?",
-        },
-      ])
-      .then((answers) => {
-        const manager = new Manager(
-          answers.managerName,
-          answers.managerID,
-          answers.email,
-          answers.officeNum
-        );
-        teamMembers.push(manager);
-        // Below this, do i need to idArray.push for each answer?
-        idArray.push(answers.id);
-        createTeam();
-      });
-  }
-
-  // Next the user will be given the choices to add an engineer, intern, or finish adding to their team.
-
-  function createTeam() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "promptType",
-          message: "What type of employee would you like to add?",
-          choices: ["Engineer", "Intern", "I am done adding members to my team."],
-        },
-      ])
-      .then((answer) => {
-        if (answer.promptType === "Engineer") {
-          addEngineer();
-        } else if (answer.promptType === "Intern") {
-          addIntern();
-        } else {
-          buildTeam();
-        }
-      });
-  }
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your manager's employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your manager's current email address?",
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        message: "What is your manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.managerName,
+        answers.managerID,
+        answers.email,
+        answers.officeNum
+      );
+      teamMembers.push(manager);
+      // Below this, do i need to idArray.push for each answer?
+      idArray.push(answers.id);
+      createTeam();
+    });
+}
 
 
-  // Below is the function to prompt the user to add an engineer
-  createEngineer();
-  function createEngineer() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is the engineer's name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your engineer's employee ID number?",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your engineer's current email address?",
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "What is your engineer's GitHub username?",
-        },
-      ])
-      .then((answers) => {
-        const engineer = new Engineer(
-          answers.engineerName,
-          answers.engineerID,
-          answers.engineerEmail,
-          answers.github
-        );
-        teamMembers.push(engineer);
-        // do I push answers.id for the manager, engineer, and intern? Or is there a separate parameter I should push in the parentheses below?
-        idArray.push(answers.id);
-        createTeam();
-      });
-  }
+// Next the user will be given the choices to add an engineer, intern, or finish adding to their team.
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "promptType",
+        message: "What type of employee would you like to add?",
+        choices: ["Engineer", "Intern", "I am done adding members to my team."],
+      },
+    ])
+    .then((answer) => {
+      if (answer.promptType === "Engineer") {
+        addEngineer();
+      } else if (answer.promptType === "Intern") {
+        addIntern();
+      } else {
+        buildTeam();
+      }
+    });
+}
 
-  // Below is the function and code for adding an Intern to the user's team
-  createIntern();
-  function createIntern() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is the intern's name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "What is your intern's employee ID number?",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your engineer's current email address?",
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "What school is the intern currently attending?",
-        },
-      ])
-      .then((answers) => {
-        const intern = new Intern(
-          answers.internName,
-          answers.internID,
-          answers.internEmail,
-          answers.school
-        );
-        teamMembers.push(intern);
-        // do I push answers.id for the manager, engineer, and intern? Or is there a separate parameter I should push in the parentheses below?
-        idArray.push(answers.id);
-        createTeam();
-      });
-  }
+// Below is the function to prompt the user to add an engineer
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your engineer's employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your engineer's current email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your engineer's GitHub username?",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.engineerName,
+        answers.engineerID,
+        answers.engineerEmail,
+        answers.github
+      );
+      teamMembers.push(engineer);
+      // do I push answers.id for the manager, engineer, and intern? Or is there a separate parameter I should push in the parentheses below?
+      idArray.push(answers.id);
+      createTeam();
+    });
+}
 
 
-
-  // // Coder/Engineer Prompt
-  // {
-  //     type: "input",
-  //     name: "engineeerName",
-  //     message: "What is the name of this engineer?"
-  // },
-  // {
-  //     type: 'input',
-  //     name: 'id',
-  //     message: 'What is your employee ID number?',
-  // },
-  // {
-  //       type: "input",
-  //       name: "email",
-  //       message: " What is your current email addres?"
-  // },
-  // {
-  //   type: "input",
-  //   name: "github",
-  //   message: " What is your GitHub username?"
-  // },
-
-  // //Intern Prompt
-  // {
-  //     type: "input",
-  //     name: "internName",
-  //     message: "What is the name of this team's intern?"
-  // },
-  // {
-  //     type: 'input',
-  //     name: 'id',
-  //     message: 'What is your employee ID number?',
-  // },
-  // {
-  //       type: "input",
-  //       name: "email",
-  //       message: " What is your current email addres?"
-  // },
-  // {
-  //   type: "input",
-  //   name: "school",
-  //   message: " What school is the intern currently attending?"
-  // }
+// Below is the function and code for adding an Intern to the user's team
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your intern's employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your engineer's current email address?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school is the intern currently attending?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.internName,
+        answers.internID,
+        answers.internEmail,
+        answers.school
+      );
+      teamMembers.push(intern);
+      // do I push answers.id for the manager, engineer, and intern? Or is there a separate parameter I should push in the parentheses below?
+      idArray.push(answers.id);
+      createTeam();
+    });
 }
 
 function buildTeam() {
@@ -216,4 +188,4 @@ function buildTeam() {
   );
 }
 
-appMenu();
+appPrompt();
